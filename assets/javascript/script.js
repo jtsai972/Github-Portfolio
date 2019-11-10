27f02b86c8d962f6020d8e9156388033128011d8
J Tsai <jtsai972@gmail.com>
2019-11-10 05:15:46 -0600

    making a smooth size toggle and new button setup

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,74 +1,88 @@
 let galleryMax = 270;
 /* On clicking a navigation item */
-$("nav a").on("click", function() { 
-    setActive($(this));
-});  
-
-/* generating palette background colors */
-genPalette(); 
-
-//Show/hide content
-$("#portfolio .more").on("click", function() {
-    let gallery = $(this).parent();
-    console.log(gallery);
-
-    gallery.css("max-height", "100%");
-    $(this).hide();
+$("nav a").on("click", function () {
+  setActive($(this));
 });
 
-function chkPortfolio() {
-    if($("#portfolio").attr("class") === "active") {
-        let test = $("#portfolio .gallery");
-        console.log(test);
-    
-        test.each(function() {
-            var $this = $(this);
-            var galleryHeight = $this.outerHeight();
-            console.log(galleryHeight);
+/* generating palette background colors */
+genPalette();
 
-            if(galleryHeight > galleryMax) {
-                $this.css("max-height", galleryMax)
-                $this.find(".more").show();
-            }
-        });
-    }
+//Show/hide content
+$("#portfolio .more").on("click", function () {
+  let gallery = $(this).parent(),
+    animateTime = 500;
+  // console.log(gallery);
+
+  if (gallery.hasClass("active")) {
+    autoHeightAnimate(gallery, animateTime);
+  } else {
+    gallery.stop().animate({ height: '16em' }, animateTime);
+  }
+
+  gallery.toggleClass("active");
+});
+
+/* Function to animate height: auto */
+function autoHeightAnimate(element, time) {
+  var curHeight = element.height(), // Get Default Height
+    autoHeight = element.css('height', 'auto').height(); // Get Auto Height
+  element.height(curHeight); // Reset to Default Height
+  element.stop().animate({ height: autoHeight }, time); // Animate to Auto Height
 }
+
+// function chkPortfolio() {
+//     if($("#portfolio .gallery").attr("class") === "active") {
+//         let test = $("#portfolio .gallery");
+//         console.log(test);
+
+//         test.each(function() {
+//             var $this = $(this);
+//             var galleryHeight = $this.outerHeight();
+//             console.log(galleryHeight);
+
+//             if(galleryHeight > galleryMax) {
+//                 $this.css("max-height", galleryMax)
+//                 $this.find(".more").show();
+//             }
+//         });
+//     }
+// }
 
 
 
 /* Generating palette colors */
 function genPalette() {
-    /* for each .color in a .palette */
-    $(".palette .color").each( function() {
-        /* set the background color to the text inside it */
-        $(this).css("background-color", $(this).text());
-    });
+  /* for each .color in a .palette */
+  $(".palette .color").each(function () {
+    /* set the background color to the text inside it */
+    $(this).css("background-color", $(this).text());
+  });
 }
 
 function setActive(navItem) {
-    /* making sure no other classes are "active" */
-    $("nav a").removeClass("active");
-    $("section").removeClass("active");
+  /* making sure no other classes are "active" */
+  $("nav a").removeClass("active");
+  $("section").removeClass("active");
 
-    //making sure portfolio stuff is hidden
-    $("#portfolio .gallery").each( function() {
-        $(this).css("max-height", galleryMax);
-        $("#portfolio .more").show();
-    })
-    
-    /* setting this anchor tag to active  */
-    navItem.addClass("active");
-    console.log(DataTransferItem);
-    
-    /* getting the id of the related section */
-    let section = navItem.attr("href");
-    
-    /* making sure we got the right thing */
-    console.log("Section:" + section);
+  // //making sure portfolio stuff is hidden
+  // $("#portfolio .gallery").each( function() {
+  //     $(this).css("max-height", galleryMax);
+  //     $("#portfolio .more").show();
+  // })
 
-    /* make the relative section active (to show content) */
-    $(section).addClass("active");
+  /* setting this anchor tag to active  */
+  navItem.addClass("active");
+  console.log(DataTransferItem);
 
-    /* If Portfolio is active */
-    if(section === "#portfolio") { chkPortfolio(); }
+  /* getting the id of the related section */
+  let section = navItem.attr("href");
+
+  /* making sure we got the right thing */
+  console.log("Section:" + section);
+
+  /* make the relative section active (to show content) */
+  $(section).addClass("active");
+
+  /* If Portfolio is active */
+  // if(section === "#portfolio") { chkPortfolio(); }
 }
