9d7ac296a2343c5240dcd9920b2013ca06436748
J Tsai <jtsai972@gmail.com>
2019-12-04 23:15:17 -0600

    included scrolling on see more

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 let galleryMax = 270;
 /* On clicking a navigation item */
 $("nav a").on("click", function () {
-  setActive($(this));
+    setActive($(this));
 });
 
 /* generating palette background colors */
@@ -9,60 +9,63 @@ genPalette();
 
 //Show/hide content
 $("#portfolio .more").on("click", function () {
-  let gallery = $(this).parent(),
-    animateTime = 500;
-  // console.log(gallery);
+    let gallery = $(this).parent(),
+        animateTime = 500;
+    // console.log(gallery);
 
-  gallery.toggleClass("active");
-  if (gallery.hasClass("active")) {
-    autoHeightAnimate(gallery, animateTime);
-  } else {
-    gallery.stop().animate({ height: '16em' }, animateTime);
-  }
+    gallery.toggleClass("active");
+    if (gallery.hasClass("active")) {
+        autoHeightAnimate(gallery, animateTime);
+        $('html, body').animate({scrollTop: '+=300px'}, 800);
+    } else {
+        gallery.stop().animate({ height: '16em' }, animateTime);
+        $('html, body').animate({scrollTop: '-=300px'}, 800);
+    }
+    
 });
 
 /* Function to animate height: auto */
 function autoHeightAnimate(element, time) {
-  var curHeight = element.height(), // Get Default Height
-    autoHeight = element.css('height', 'auto').height(); // Get Auto Height
-  element.height(curHeight); // Reset to Default Height
-  element.stop().animate({ height: autoHeight }, time); // Animate to Auto Height
+    var curHeight = element.height(), // Get Default Height
+        autoHeight = element.css('height', 'auto').height(); // Get Auto Height
+    element.height(curHeight); // Reset to Default Height
+    element.stop().animate({ height: autoHeight }, time); // Animate to Auto Height
 }
 
 
 /* Generating palette colors */
 function genPalette() {
-  /* for each .color in a .palette */
-  $(".palette .color").each(function () {
-    /* set the background color to the text inside it */
-    $(this).css("background-color", $(this).text());
-  });
+    /* for each .color in a .palette */
+    $(".palette .color").each(function () {
+        /* set the background color to the text inside it */
+        $(this).css("background-color", $(this).text());
+    });
 }
 
 function setActive(navItem) {
-  /* making sure no other classes are "active" */
-  $("nav a").removeClass("active");
-  $("section").removeClass("active");
+    /* making sure no other classes are "active" */
+    $("nav a").removeClass("active");
+    $("section").removeClass("active");
 
-  // //making sure portfolio stuff is hidden
-  // $("#portfolio .gallery").each( function() {
-  //     $(this).css("max-height", galleryMax);
-  //     $("#portfolio .more").show();
-  // })
+    // //making sure portfolio stuff is hidden
+    // $("#portfolio .gallery").each( function() {
+    //     $(this).css("max-height", galleryMax);
+    //     $("#portfolio .more").show();
+    // })
 
-  /* setting this anchor tag to active  */
-  navItem.addClass("active");
-  console.log(DataTransferItem);
+    /* setting this anchor tag to active  */
+    navItem.addClass("active");
+    console.log(DataTransferItem);
 
-  /* getting the id of the related section */
-  let section = navItem.attr("href");
+    /* getting the id of the related section */
+    let section = navItem.attr("href");
 
-  /* making sure we got the right thing */
-  console.log("Section:" + section);
+    /* making sure we got the right thing */
+    console.log("Section:" + section);
 
-  /* make the relative section active (to show content) */
-  $(section).addClass("active");
+    /* make the relative section active (to show content) */
+    $(section).addClass("active");
 
-  /* If Portfolio is active */
-  // if(section === "#portfolio") { chkPortfolio(); }
+    /* If Portfolio is active */
+    // if(section === "#portfolio") { chkPortfolio(); }
 }
